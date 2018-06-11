daa8d2a0045cf2f0831706551fb9002b530f3d44
Iulia <juliagatea@yahoo.com>
2018-06-11 10:27:39 +0000

    Add bootstrap styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,15 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_tenant!, :only => [ :index ]
 
   def index
+    if current_user
+      if session[:tenant_id]
+        Tenant.set_currenct_tenant session[:tenant_id]
+      else
+        Tenane.set_currenct_tenant current_user.tenants.first
+      end
+      
+      @tenant = Tenant.current_tenant
+      params[:tenant_id] = @tenant.id
+    end
   end
 end
